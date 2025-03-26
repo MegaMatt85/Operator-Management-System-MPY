b449a674ba504589b0e2e0c19588777f9687b39a
Matt <mattpaewai@gmail.com>
2025-03-26 14:45:44 +1300

    changed searchOperators to print the first operator that has correctly been searched for

--- src/main/java/nz/ac/auckland/se281/OperatorManagementSystem.java
+++ src/main/java/nz/ac/auckland/se281/OperatorManagementSystem.java
@@ -22,28 +22,29 @@ public class OperatorManagementSystem {
       MessageCli.OPERATOR_NOT_CREATED_INVALID_LOCATION.printMessage(location);
       return false;
     }
-
+    // If operator is valid
     return true;
   }
 
   public void searchOperators(String keyword) {
     // Goes through savedOperators counting how many of them include
-    int operatorsFound = 0;
+    ArrayList<Integer> operatorsFound = new ArrayList<Integer>();
     // String foundOperator = "";
     for (int i = 0; i < savedOperators.size(); i++) {
       if (keyword.equals("*")) {
-        operatorsFound++;
+        operatorsFound.add(i);
       }
     }
 
-    if (operatorsFound == 0) {
+    if (operatorsFound.size() == 0) {
       // Prints for no operators
       MessageCli.OPERATORS_FOUND.printMessage("are", "no", "s", ".");
-    } else if (operatorsFound == 1) {
+    } else if (operatorsFound.size() == 1) {
       // Prints for one operator
        MessageCli.OPERATORS_FOUND.printMessage("is", "1", "", ":");
-       System.out.println("* " + savedOperators.get(0).getName() + " ('" + savedOperators.get(0).getId() + "' located in '" 
-         + savedOperators.get(0).getLocationFull() + "')");
+       System.out.println("* " + savedOperators.get(operatorsFound.get(0)).getName() + " ('" 
+        + savedOperators.get(operatorsFound.get(0)).getId() + "' located in '" 
+        + savedOperators.get(operatorsFound.get(0)).getLocationFull() + "')"); // TO BE UPDATED
     } else {
       // Prints for two or more operators found
       MessageCli.OPERATORS_FOUND.printMessage("are", Integer.toString(2), "s", ":");
