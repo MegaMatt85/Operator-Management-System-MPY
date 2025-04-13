ebb100304be3c68cd3cbdbe8ff7a58031f6ec10d
Matt <mattpaewai@gmail.com>
2025-04-13 21:41:29 +1200

    Added ExpertReview class to be able to create expert reviews and passed test cases 4,6, and 7

---
+++ src/main/java/nz/ac/auckland/se281/ExpertReview.java
@@ -0,0 +1,30 @@
+package nz.ac.auckland.se281;
+
+public class ExpertReview extends Review {
+  Boolean reccomend;
+
+  ExpertReview(String[] options, Activity activity) {
+    super(activity);
+    super.name = options[0];
+    super.rating = Integer.parseInt(options[1]);
+    super.comment = options[2];
+
+    if (super.rating < 0) {
+      super.rating = 0;
+    } else if (this.rating > 5) {
+      super.rating = 5;
+    }
+
+    if (options[3].equals("y")) {
+      this.reccomend = true;
+    } else {
+      this.reccomend = false;
+    }
+  }
+
+  @Override
+  public void printSuccess() {
+    MessageCli.REVIEW_ADDED.printMessage("Expert", 
+    super.reviewId, super.activity.getName());
+  }
+}
